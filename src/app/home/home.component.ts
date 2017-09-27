04c620818380b4ceaa4e15d82cc9ae41469c2370
Michail Schwab <me@michaschwab.de>
2017-09-27 17:59:22 -0400

    some initial fixes for mobile

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,27 @@
-import {Component} from '@angular/core';
+import {AfterViewChecked, Component, ElementRef, ViewChild} from '@angular/core';
 
 @Component({
   selector: "home",
   templateUrl: "home.component.html",
   styleUrls: ["./home.component.scss"]
 })
-export class HomeComponent
+export class HomeComponent implements AfterViewChecked
 {
+    @ViewChild("homeContainer") homeContainer : ElementRef;
+    
+    width: number;
+    timelineWidth = 400;
+    
     showCode = {
         0: false,
         1: false,
         2: false,
         3: false,
+    };
+    
+    ngAfterViewChecked(): void
+    {
+        this.width = this.homeContainer.nativeElement.offsetWidth;
+        this.timelineWidth = this.width < 1000 ? this.width - 30 : 400;
     }
 }

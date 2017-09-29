c57927b9588f0c8efccd66514cf0bc7dac410bf3
Michail Schwab <me@michaschwab.de>
2017-09-29 14:02:09 -0400

    got the navigation to work

--- src/app/app/app.component.ts
+++ src/app/app/app.component.ts
@@ -7,8 +7,18 @@ import {ZoomComponent} from '../zoomable/zoomable.component';
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
-export class AppComponent
+export class AppComponent implements AfterViewInit
 {
-
-
+    activeTab = 'library';
+    
+    ngAfterViewInit(): void
+    {
+        window.addEventListener('scroll', () =>
+        {
+            const scrollPos = window.pageYOffset + 67;
+            const researchPos = document.getElementById('research').offsetTop;
+            
+            this.activeTab = scrollPos >= researchPos ? 'research' : 'library';
+        });
+    }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,11 @@
-import {AfterViewChecked, Component, ElementRef, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
 
 @Component({
   selector: "home",
   templateUrl: "home.component.html",
   styleUrls: ["./home.component.scss"]
 })
-export class HomeComponent implements AfterViewChecked
+export class HomeComponent implements AfterViewInit
 {
     @ViewChild("homeContainer") homeContainer : ElementRef;
     
@@ -18,8 +18,8 @@ export class HomeComponent implements AfterViewChecked
         2: false,
         3: false,
     };
-    
-    ngAfterViewChecked(): void
+
+    ngAfterViewInit(): void
     {
         this.width = this.homeContainer.nativeElement.offsetWidth;
         this.timelineWidth = this.width < 1000 ? this.width - 30 : 420;

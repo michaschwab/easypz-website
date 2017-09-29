efdb92fff9ff4ef61e65ff35092eff5e01abc117
Michail Schwab <me@michaschwab.de>
2017-09-29 10:18:14 -0400

    working on showing research results

---
+++ src/app/research/research.component.ts
@@ -0,0 +1,27 @@
+import {AfterViewChecked, Component, ElementRef, ViewChild} from '@angular/core';
+
+@Component({
+  selector: "research",
+  templateUrl: "research.component.html",
+  styleUrls: ["./research.component.scss"]
+})
+export class ResearchComponent implements AfterViewChecked
+{
+    @ViewChild("researchContainer") researchContainer : ElementRef;
+    
+    width: number;
+    timelineWidth = 400;
+    
+    showCode = {
+        0: false,
+        1: false,
+        2: false,
+        3: false,
+    };
+    
+    ngAfterViewChecked(): void
+    {
+        this.width = this.researchContainer.nativeElement.offsetWidth;
+        this.timelineWidth = this.width < 1000 ? this.width - 30 : 400;
+    }
+}

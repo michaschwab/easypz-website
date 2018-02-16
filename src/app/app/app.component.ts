632e6a3182b72390de68ae489e25cb24eddb7825
michaschwab <me@michaschwab.de>
2018-02-16 14:55:45 -0500

    put the intro gif and text side by side

--- src/app/app/app.component.ts
+++ src/app/app/app.component.ts
@@ -1,6 +1,5 @@
 import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
-import {CodableTimelineComponent} from '../codable-timeline/codable-timeline.component';
-import {ZoomComponent} from '../zoomable/zoomable.component';
+
 /* tslint:disable */
 @Component({
     selector: 'app-root',
@@ -9,22 +8,26 @@ import {ZoomComponent} from '../zoomable/zoomable.component';
 })
 export class AppComponent implements AfterViewInit
 {
+    @ViewChild("appContainer") appContainer: ElementRef;
+    width: number;
     activeTab = '';
-    
+
     ngAfterViewInit(): void
     {
+        this.width = this.appContainer.nativeElement.offsetWidth;
+
         window.addEventListener('scroll', () =>
         {
             const sections = ['showcase', 'library', 'about'];
             const scrollPos = window.pageYOffset + 67;
-            
+
             const sectionPositions = sections.map(sectionName =>
                 document.getElementById(sectionName).offsetTop
             );
             sectionPositions.push(1000000); // Infinity
-    
+
             this.activeTab = '';
-            
+
             sections.forEach((sectionName, index) =>
             {
                 if(scrollPos >= sectionPositions[index] && scrollPos <= sectionPositions[index + 1])
@@ -34,7 +37,7 @@ export class AppComponent implements AfterViewInit
             });
         });
     }
-    
+
     scrollTo(anchorName: string)
     {
         //let anchor = document.getElementsByName(anchorName)[0];
